2bb35dc83923c39ebc198ac757deb718089f6d41
Mehul Kohli <mkohli1997@gmail.com>
2020-12-21 14:54:02 -0800

    Update filter_emoticon_data.py

--- filter_emoticon_data.py
+++ filter_emoticon_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 from tqdm import tqdm
 import demoji
 
+TWEETS_FILEPATH = "<path to the extracted tweets csv file>"
+OUTFILE = "<path for output file>"
 
 def get_emoji(text):
     """
@@ -16,18 +18,18 @@ def get_emoji(text):
 
 def remove_emoji(text):
     """
-    This function removes all special characters that may or may not represent an emoticon.
+    This function removes all special characters that may or may not represent an emoticon and convert to lower case.
     :param text: tweet text
     :return: cleaned text
     """
-    return ''.join([ch for ch in text if ch.isalnum() or ch == " "])
+    return ''.join([ch for ch in text if ch.isalnum() or ch == " "]).lower()
 
 
 if __name__ == "__main__":
-    df_tweets = pd.read_csv("tweets_730k_cleaned.csv", encoding="utf-8")
+    df_tweets = pd.read_csv(TWEETS_FILEPATH, encoding="utf-8")
     print("Looking for emoticons...")
     tqdm.pandas()
     df_tweets["emoticons"] = df_tweets["text"].progress_apply(get_emoji)
     print("Cleaning the text...")
     df_tweets["text"] = df_tweets["text"].progress_apply(remove_emoji)
-    df_tweets.to_csv("tweets_emoticons_processed.csv", index=False)
+    df_tweets.to_csv(OUTFILE, index=False)
